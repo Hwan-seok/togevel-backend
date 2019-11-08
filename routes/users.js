aad6fcf9881b261b10b122a22c3b25e3fc12d6be
Hwan-seok <tttkhs@khu.ac.kr>
2019-11-08 22:39:56 +0900

    - login register

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
-var express = require('express');
+var express = require("express");
+const { generateToken, verifyToken } = require("../modules/jwt.util");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post("/register", function(req, res, next) {
+    console.log(req.body);
+    const accessToken = generateToken(req.body);
+    res.status(200).json({ accessToken });
 });
 
+/* GET users listing. */
+router.post("/login", async (req, res, next) => {
+    console.log(req.body);
+    const verified = await verifyToken(req.body.accessToken);
+    console.log("asdf,", verified);
+    res.status(200).json({ result: verified });
+});
 module.exports = router;
