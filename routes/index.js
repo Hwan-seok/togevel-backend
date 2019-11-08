a32786142e5a57062bf3176186efc86aa33640b0
Hwan-seok <tttkhs@khu.ac.kr>
2019-11-08 19:02:54 +0900

    - server up

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+    res.status(200).json({
+        result: "success"
+    });
 });
+router.get("/gotoTomorrow",()=>{
+    
+})
 
 module.exports = router;
