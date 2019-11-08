7f02ba67bd998e80d45406a9273f200bc801f7f8
Hwan-seok <tttkhs@khu.ac.kr>
2019-11-09 01:10:03 +0900

    - cors err resolve

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
+const cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
